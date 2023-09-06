a232b8d73c1c10cd0f1a25d51fe8a15f8880b4e1
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-09-06 17:52:45 +0200

    add gcs as backend for state

--- provider.tf
+++ provider.tf
@@ -2,3 +2,17 @@ provider "google" {
   project = "test-project"
   region = "us-central1"
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "bucket-name"
+    prefix = "terraform/statefile"
+  }
+
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "~> 4.0"
+    }
+  }
+}
